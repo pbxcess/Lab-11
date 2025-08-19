ac74f790cce506821e69edf10177a58a16629f39
Princess Krish Anne Bernardino <pkabernardino@gmail.com>
2025-08-19 00:47:37 -0400

    Test and Amend: Added sample products to very applyDiscount and amended a coding error leading to console output error

--- script.js
+++ script.js
@@ -15,18 +15,21 @@ class Product {
     }
 
     static applyDiscount(products, discount) {
-        if (!Array.isArray(products)) throw new TypeError("prodtcs must be an array");
-        if (typeof discount !== "number" || discount < 0 || discount > 0.9);
+        if (!Array.isArray(products)) {
+            throw new TypeError("products must be an array");
+        }
+        if (typeof discount !== "number" || discount < 0 || discount > 0.9) {
             throw new RangeError("discount should be a decimal between 0 and 0.9");
-    }
-    for (const p of products) {
-        if (!(p instanceof Product)) continue;
-        p.price = Number((p.price * (1 - discount)).toFixed(2));
+        }
+
+        for (const p of products) {
+            if (!(p instanceof Product)) continue;
+            p.price = Number((p.price * (1 - discount)).toFixed(2));
+        }
     }
 }
 
-const testProduct = new Product("Laptop", 1399.99, 5);
-console.log(testProduct.toString(), "Total Value:", testProduct.getTotalValue());
+
 
 //Part 2:
 class PerishableProduct extends Product {
@@ -40,7 +43,19 @@ class PerishableProduct extends Product {
     }
 }
 
+// Test Runs
+
+const testProduct = new Product("Laptop", 1399.99, 5);
+console.log(testProduct.toString(), "Total Value:", testProduct.getTotalValue());
+
+
 const milk = new PerishableProduct("Milk", 5.69, 30, "2025-09-18");
 const bread = new PerishableProduct("Bread", 2.99, 40, "2025-08-30");
 console.log(milk.toString());
 console.log(bread.toString());
+
+const headphones = new Product("Headphones", 149.99, 20);
+const sampleArr = [testProduct, milk, headphones];
+console.log("Before discount:", sampleArr.map(p => p.price));
+Product.applyDiscount(sampleArr, 0.1);
+console.log("After 10% discount:", sampleArr.map(p => p.price));
