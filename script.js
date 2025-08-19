66e8144ec364d349c34771a173e1d0e58b7b62e0
Princess Krish Anne Bernardino <pkabernardino@gmail.com>
2025-08-19 00:14:16 -0400

    Feat: Add Product class with constructor

---
+++ script.js
@@ -0,0 +1,8 @@
+//Part 1
+class Product {
+    constructor(name, price, quantity) {
+        this.name = String(name);
+        this.price = Number(price);
+        this.quantity = Number(quantity);
+    }
+}
