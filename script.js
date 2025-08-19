6dd830f0040e4c6dc9ccdfceca67e6724bc9e8c1
Princess Krish Anne Bernardino <pkabernardino@gmail.com>
2025-08-19 00:25:02 -0400

    Feat: Override toString in the PerishableProduct

--- script.js
+++ script.js
@@ -25,4 +25,8 @@ class PerishableProduct extends Product {
         super(name, price, quantity);
         this.expirationDate = String(expirationDate);
     }
+
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2),}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
+    }
 }
